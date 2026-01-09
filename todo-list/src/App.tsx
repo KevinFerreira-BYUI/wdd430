a7807369f91d3b3db71f253a63ec84aac3a48d53
Kevin G. Ferreira <kevingarciaf.kf@outlook.com>
2026-01-08 21:57:11 -0300

    week 1 assigment learning react

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,35 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { use, useState } from 'react'
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [newItem, setNewItem] = useState("")
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    interface todoProps{
+        id: string,
+        title: string,
+        completed: boolean    
+    }
+
+    const [toDo, setTodo] = useState<todoProps[]>([])
+    
+    function toDos(e: React.FormEvent<HTMLFormElement>){
+        e.preventDefault()
+
+        setTodo(insertTodo => {
+            return[
+                ...insertTodo,
+                {
+                    id: crypto.randomUUID(),
+                    title: newItem,
+                    completed: false
+                }
+            ]
+        })
+    }
+
+    function toggleTodo(id:string, completed:boolean){
+        setTodo(insertTodo => {
+            return insertTodo.map(todo => {
+                if(todo.id === id){
+                    return {...todo, completed}
+                }
+
+                return todo
+            })
+        })
+    }
+
+    function deleteItem(id:string){
+        setTodo(insertTodo => {
+            return insertTodo.filter(todo => todo.id !== id)
+        })
+    }
+
+    return (
+        <>
+            <section className="d-flex flex-column align-items-center">
+                <div>
+                    <form className="todoForm d-flex flex-column mb-3" onSubmit={toDos}>
+                        <h1>New Item</h1>
+                        <input id="insertTodo" type="text" value={newItem} onChange={e => setNewItem(e.target.value)}/>  
+                        <input id="submitTodo" className="mt-3 btn btn-primary btn-lg" type="submit" value="Add"/>
+                    </form>
+                </div>
+
+                <div>
+                    <h1 className='p-4'>To do List</h1>
+                    <div className='list-container mt-4'>
+                        <ul className="list">
+                            {toDo.map(todo => {
+                                return <li key={todo.id}>
+                                    <label className="me-3">
+                                        <input
+                                        className="checkBox me-2" 
+                                        type="checkbox" 
+                                        checked={todo.completed}
+                                        onChange={e => toggleTodo(todo.id, e.target.checked)}
+                                        />
+                                        {todo.title}
+                                    </label>
+                                    <button className='btn btn-danger' onClick={() => deleteItem(todo.id)}>Delete</button>
+                                </li>
+                            })}
+                        </ul>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
 }
 
 export default App
+  
