af0088bf2ce20384614ab5169d26522cf7b432c5
Kevin G. Ferreira <kevingarciaf.kf@outlook.com>
2026-01-15 20:32:39 -0300

    reformulating

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,17 +1,11 @@
-// import "./globals.css";
-
-
-export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
-    return(
-        <html lang="en">
-            <head>
-                {/* <meta charset="UTF-8" /> */}
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <title>Document</title>
-            </head>
-            <body>
-                {children}
-            </body>
-        </html>
-    )
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
